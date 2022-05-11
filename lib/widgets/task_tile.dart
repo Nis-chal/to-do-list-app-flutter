214c850b88ae43fee70496fa4608d0cea9a5a03b
Nis-chal <nischalchan@gmail.com>
2022-05-11 13:03:43 +0545

    now user can toggle checkbox value

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,13 +1,60 @@
 import 'package:flutter/material.dart';
+
 class TaskTile extends StatelessWidget {
+  final bool isChecked;
+  final String taskTitle;
+  final void Function(bool?) checkboxCallback;
+
+  TaskTile(
+      {required this.isChecked,
+      required this.taskTitle,
+      required this.checkboxCallback});
+
+  //   void checkboxCallback(bool? checkboxState) {
+  //   setState(() {
+  //     isChecked = checkboxState!;
+  //   });
+  // }
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('This is a task'),
-      trailing: Checkbox(
-        value: false,
-        onChanged: (value) {},
-      ),
-    );
+        title: Text(
+          taskTitle,
+          style: TextStyle(
+              decoration: isChecked ? TextDecoration.lineThrough : null),
+        ),
+        trailing: Checkbox(
+          activeColor: Colors.lightBlueAccent,
+          value: isChecked,
+          onChanged: checkboxCallback,
+          // onChanged: toggleCheckboxState,
+        ));
   }
 }
+
+// class TaskCheckbox extends StatelessWidget {
+//   final bool checkboxState;
+//   final void Function(bool?) toggleCheckboxState;
+
+//   TaskCheckbox(
+//       {required this.checkboxState, required this.toggleCheckboxState});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Checkbox(
+//       activeColor: Colors.lightBlueAccent,
+//       value: checkboxState,
+//       onChanged: toggleCheckboxState,
+//     );
+//   }
+// }
+
+
+//  (bool? checkboxState) {
+//             setState(() {
+//               isChecked = checkboxState!;
+//             });
+//           }),
+
+
